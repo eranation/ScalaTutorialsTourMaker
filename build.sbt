65d8151bcef3ffe251d7207ec6c666ae63f21d53
Eran Medan <ehrann.mehdan@gmail.com>
2013-07-29 18:52:14 -0400

    initial commit

--- build.sbt
+++ build.sbt
@@ -1,5 +1,17 @@
-name := "Hello"
+name := "ScalaTutorialsTourMaker"
+
+description := "A utility for creating scalatutorials.com's 'tour of scala' pages from a CSV file"
 
 version := "1.0"
 
 scalaVersion := "2.10.2"
+
+libraryDependencies += "org.fusesource.scalate" % "scalate-core_2.10" % "1.6.1"
+
+libraryDependencies += "org.slf4j" % "slf4j-log4j12" % "1.7.5"
+
+libraryDependencies += "net.sf.opencsv" % "opencsv" % "2.3"
+
+libraryDependencies += "org.yaml" % "snakeyaml" % "1.12"
+
+
---
+++ src/main/scala/ScalaTutorialTourMaker.scala
@@ -0,0 +1,67 @@
+import au.com.bytecode.opencsv.CSVReader
+import java.io.{File, FileOutputStream, InputStreamReader}
+import java.util._
+import javax.swing.text.AbstractDocument.Content
+import org.fusesource.scalate._
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+
+object ScalaTutorialTourMaker extends App {
+
+  val cl = Thread.currentThread().getContextClassLoader
+  val props = new Properties
+  props.load(cl.getResourceAsStream("config.properties"))
+
+  val engine = new TemplateEngine
+
+
+  val reader = new CSVReader(new InputStreamReader(cl.getResourceAsStream("tour.csv")))
+
+  val headers = reader.readNext()
+  val lines = reader.readAll()
+  val totalLines = lines.length
+  val files = for ((line, i) <- lines.zipWithIndex) yield {
+    val map = line.zipWithIndex.map {
+      case (col, j) => headers(j) -> col
+    }.toMap.updated("isLast", i == totalLines - 1).updated("pageNumber", i + 1)
+
+    //val output = engine.layout("template.ssp", Map("title" -> "My title"))
+
+    val title: String = map("title").asInstanceOf[String] //ugly but quick
+    val slug = title.toLowerCase.replaceAll(" ", "_").replaceAll("\\W", "")
+    val fileName = s"${i + 1}_tour_of_scala_$slug"
+    (fileName, map)
+    //println(output)
+    //println(s"line $i, col $j, colName $h, colValue ${l(j)}")
+  }
+  for (((fileName, map), index) <- files.zipWithIndex) {
+    val outputFile = new File(props.getProperty("outputFolder"),fileName + ".md")
+
+    printToFile(outputFile)(p => {
+      val params = mutable.HashMap[String, Any]()
+      params ++= map
+      if (index < totalLines - 1) {
+        params += "nextPage" -> (files(index + 1)._1 + ".html")
+      }
+
+      if (index > 0) {
+        params += "prevPage" -> (files(index - 1)._1 + ".html")
+      }
+
+      val output = engine.layout("template.ssp", params.toMap)
+
+
+
+      p.print(output)
+    })
+  }
+
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try {
+      op(p)
+    } finally {
+      p.close()
+    }
+  }
+}
